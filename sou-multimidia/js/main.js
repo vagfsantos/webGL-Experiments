ec284698da6ecd327697d1bcf85b18b96db1e07c
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-04-10 22:40:55 -0300

    trying some methods

--- sou-multimidia/js/main.js
+++ sou-multimidia/js/main.js
@@ -1,1 +1,17 @@
-alert('a');
+// Load before
+function preload(){}
+
+// Config project
+function setup(){
+  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+}
+
+// animation loop
+function draw(){
+  background(0);
+  ambientLight(235, 235, 36);
+  pointLight(250, 250, 250, 100, 100, 0);
+  specularMaterial(250);
+  translate(-width/2, -height/2, 0);
+  torus(40, 10, 40);
+}
